a10769648a68c5a78979873c547bed0816b49b17
Jack Firth <jackhfirth@gmail.com>
2022-04-29 19:59:16 -0700

    Fill out template

--- info.rkt
+++ info.rkt
@@ -1,12 +1,12 @@
 #lang info
 
-(define collection "racket-package-template")
+(define collection "sgf")
 
 (define scribblings
   (list (list "main.scrbl"
               (list 'multi-page)
               (list 'library)
-              "racket-package-template")))
+              "sgf")))
 
 (define deps
   (list "base"))
